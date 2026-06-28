fb83f98e1b9359f61f9fcd11262db9a838b09c45
Mo Vahedi <m.vahedi65@gmail.com>
2026-06-27 19:16:28 -0700

    feat: create custom 404 error page

---
+++ src/hooks/useLocalStorage.ts
@@ -0,0 +1,20 @@
+import { useState } from 'react';
+export function useLocalStorage<T>(key: string, initialValue: T) {
+  const [storedValue, setStoredValue] = useState<T>(() => {
+    try {
+      const item = window.localStorage.getItem(key);
+      return item ? JSON.parse(item) : initialValue;
+    } catch (error) {
+      return initialValue;
+    }
+  });
+  const setValue = (value: T) => {
+    try {
+      setStoredValue(value);
+      window.localStorage.setItem(key, JSON.stringify(value));
+    } catch (error) {
+      console.warn('Error setting localStorage', error);
+    }
+  };
+  return [storedValue, setValue] as const;
+}
